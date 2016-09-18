a23f0cf8eee9ddf2b33480ecd8972d1290cfa250
Qi Deng <QiDengNovember@gmail.com>
2016-09-18 23:41:45 +1000

    ADD using database and populating database records into views (Chat Tab); FATEL buttons add friend and new chat

---
+++ app/src/main/java/com/unimelb/wesnap/models/Chat.java
@@ -0,0 +1,42 @@
+package com.unimelb.wesnap.models;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+/**
+ * Created by qideng on 18/09/2016.
+ */
+@IgnoreExtraProperties
+public class Chat {
+
+    private String chatId;
+    private String receiverName; // TODO: messageImage???
+    private String receiverPhotoUrl;
+
+    public Chat() {
+    }
+
+    public Chat(String receiverName, String receiverPhotoUrl) {
+        this.receiverName = receiverName;
+        this.receiverPhotoUrl = receiverPhotoUrl;
+    }
+
+    public String getId() {
+        return chatId;
+    }
+
+    public String getReceiverName() {
+        return receiverName;
+    }
+
+    public void setReceiverName(String receiverName) {
+        this.receiverName = receiverName;
+    }
+
+    public String getReceiverPhotoUrl() {
+        return receiverPhotoUrl;
+    }
+
+    public void setReceiverPhotoUrl(String receiverPhotoUrl) {
+        this.receiverPhotoUrl = receiverPhotoUrl;
+    }
+}
