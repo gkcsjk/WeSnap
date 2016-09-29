c99b4529e4c44b859b5f92c964648aa107097a19
Karl <gkcsjk@gmail.com>
2016-09-29 12:03:52 +1000

    Revert "camera show&save image"
    This reverts commit 84f987436dbd63f3c65b4009d4d459fafc8d3a9b.

--- app/src/main/java/com/unimelb/wesnap/CameraFragment.java
+++ app/src/main/java/com/unimelb/wesnap/CameraFragment.java
@@ -1,17 +1,10 @@
 package com.unimelb.wesnap;
 
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
-import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.FileProvider;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,26 +14,18 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-
 /**
  * A placeholder fragment containing a simple view.
  */
 public class CameraFragment extends Fragment {
 
     private static final String TAG = "CameraFragment";
-    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     /* UI Variables */
     private Button cameraB;
     private OnClickListener clickListener;
     private TextView textView3;
-    private ImageView mImageView;
-    private String mCurrentPhotoPath;
+    private ImageView iv3;
 
     /* Fragment singleton??? */
     private static CameraFragment mCameraFragment = null;
@@ -58,50 +43,13 @@ public class CameraFragment extends Fragment {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == getActivity().RESULT_OK){
-            if (mCurrentPhotoPath != null){
-                setPic();
-                mCurrentPhotoPath = null;
-            }
-        }
-    }
-
-
-    private void setPic() {
-		/* Get the size of the ImageView */
-        int targetW = mImageView.getWidth();
-        int targetH = mImageView.getHeight();
-        Log.d("current path",mCurrentPhotoPath);
-		/* Get the size of the image */
-        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-        bmOptions.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
-        int photoW = bmOptions.outWidth;
-        int photoH = bmOptions.outHeight;
-
-        		/* Figure out which way needs to be reduced less */
-        int scaleFactor = 1;
-        if ((targetW > 0) || (targetH > 0)) {
-            scaleFactor = Math.min(photoW/targetW, photoH/targetH);
-        }
-
-		/* Set bitmap options to scale the image decode target */
-        bmOptions.inJustDecodeBounds = false;
-        bmOptions.inSampleSize = scaleFactor;
-        bmOptions.inPurgeable = true;
-
-		/* Decode the JPEG file into a Bitmap */
-        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
-
-		/* Associate the Bitmap to the ImageView */
-        mImageView.setImageBitmap(bitmap);
-        mImageView.setVisibility(View.VISIBLE);
+        Log.i(TAG,", resultCode=" + resultCode);
+        iv3.setImageURI(data.getData());
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        mImageView = (ImageView) view.findViewById(R.id.phview);
         textView3 = (TextView) view.findViewById(R.id.camera_txt);
         textView3.setText(R.string.text_camera);
 
@@ -109,25 +57,15 @@ public class CameraFragment extends Fragment {
         clickListener = new OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
-                    File photoFile = null;
-                    try{
-                        photoFile = createImageFile();
-                    } catch (IOException ex){
-                        Log.d("Error", "Create file error");
-                    }
-                    if (photoFile  != null) {
-                        Uri photoURI = FileProvider.getUriForFile(getActivity(),
-                                "com.unimelb.wesnap.fileprovider",
-                                photoFile);
-                        intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
-                    }
-                }
+                Intent intent = new Intent();
+                intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.addCategory(Intent.CATEGORY_DEFAULT);
+                startActivityForResult(intent, 1);
             }
         };
         cameraB.setOnClickListener(clickListener);
+
+        iv3 = (ImageView) view.findViewById(R.id.phview);
     }
 
     @Override
@@ -135,20 +73,4 @@ public class CameraFragment extends Fragment {
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_camera, container, false);
     }
-
-    private File createImageFile() throws IOException{
-        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timestamp + "_";
-        File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,
-                ".jpg",
-                storageDir
-        );
-
-        mCurrentPhotoPath = image.getAbsolutePath();
-        return image;
-    }
-
-
 }
